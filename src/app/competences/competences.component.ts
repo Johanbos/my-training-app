8a964acdaba1c783cba9ec7b33288438ba396e82
Johan Bos <jbos@snelstart.nl>
2020-11-30 16:17:01 +0100

    Color!

--- src/app/competences/competences.component.ts
+++ src/app/competences/competences.component.ts
@@ -1,4 +1,4 @@
-import { HostListener, Input } from '@angular/core';
+import { EventEmitter, HostListener, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Competence } from '../shared/model/competence';
 import { Training } from '../shared/model/training';
@@ -11,23 +11,33 @@ import { Training } from '../shared/model/training';
 
 export class CompetencesComponent implements OnInit {
   @Input() list: Competence[] = [];
-
+  backgroundColor = 'auto';
+  @Output() backgroundColorChange = new EventEmitter<string>();
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
   celebrate(competence: Competence): void {
-    alert(competence.name + " is awesome");
+    alert(`${competence.name} is awesome`);
   }
 
   delete(competence: Competence): void {
     this.list = this.list.filter(item => item.name != competence.name);
   }
 
-  onKeyUp(input: HTMLInputElement) {
+  onAdd(input: HTMLInputElement) {
     if (input.value != '') {
       this.list.push({ name: input.value });
+      input.value = '';
+    }
+  }
+
+  onColor(input: HTMLInputElement) {
+    if (input.value != '') {
+      this.backgroundColor = input.value;
+      this.backgroundColorChange.emit(input.value)
       input.value = '';
     }
   }
