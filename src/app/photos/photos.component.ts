cb3e464636963769a8d4071c1b78feed9124aae8
Johan Bos <jbos@snelstart.nl>
2020-12-01 15:06:09 +0100

    Fix photos location

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { PhotoService } from '../app/shared/service/photo.service';
+import { PhotoService } from '../shared/service/photo.service';
 
 @Component({
   selector: 'app-photos',
