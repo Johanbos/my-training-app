0042bfc11b7a48171c99ed554f6ab26b9fee2f05
Johan Bos <jbos@snelstart.nl>
2020-12-02 11:38:32 +0100

    Replay favorites added/deleted

--- src/app/shared/service/favorites.service.ts
+++ src/app/shared/service/favorites.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { ReplaySubject } from 'rxjs';
 import { Competence } from '../model/competence';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
-  competenceAdded$ = new Subject<Competence>();
-  competenceDeleted$ = new Subject<Competence>();
+  competenceAdded$ = new ReplaySubject<Competence>();
+  competenceDeleted$ = new ReplaySubject<Competence>();
 
   constructor() { }
 }
