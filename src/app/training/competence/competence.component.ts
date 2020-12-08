21cbc3183b12133cf29ba45052602a4c1cedb026
Johan Bos <jbos@snelstart.nl>
2020-12-08 16:03:37 +0100

    View competence clean

--- src/app/training/competence/competence.component.ts
+++ src/app/training/competence/competence.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Competence } from 'src/app/shared/model/competence';
-import { FavoritesService } from 'src/app/shared/service/favorites.service';
 
 @Component({
   selector: 'app-competence',
@@ -10,12 +9,13 @@ import { FavoritesService } from 'src/app/shared/service/favorites.service';
 })
 export class CompetenceComponent implements OnInit, OnChanges {
   @Input() competence?: Competence;
-  @Output() deleteCompetence = new EventEmitter<Competence>();
-  @Output() saveCompetence = new EventEmitter<Competence>();
+  @Output() delete = new EventEmitter<Competence>();
+  @Output() sumbit = new EventEmitter<Competence>();
+  @Output() favorite = new EventEmitter<Competence>();
+  @Output() unfavorite = new EventEmitter<Competence>();
   formGroup!: FormGroup;
 
   constructor(
-    private favoritesService: FavoritesService,
     private formBuilder: FormBuilder
   ) { }
 
@@ -30,28 +30,6 @@ export class CompetenceComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.competence && this.competence) {
       this.formGroup.setValue(this.competence);
-    }
-  }
-
-  submit() {
-    if (this.formGroup.pristine) {
-      return;
-    }
-
-    if (this.formGroup.valid) {
-      this.saveCompetence.emit(this.formGroup.value);
-    }
-  }
-
-  deleteCompetenceClick() {
-    if (this.competence) {
-      this.deleteCompetence.emit(this.competence);
-    }
-  }
-
-  favoriteCompetenceClick() {
-    if (this.competence) {
-      this.favoritesService.add(this.competence);
     }
   }
 }
--- src/app/training/competences/competences.component.ts
+++ src/app/training/competences/competences.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Competence } from 'src/app/shared/model/competence';
 import { BackgroundColorService } from 'src/app/shared/service/background-color.service';
+import { FavoritesService } from 'src/app/shared/service/favorites.service';
 
 @Component({
   selector: 'app-competences',
@@ -12,7 +13,10 @@ export class CompetencesComponent implements OnInit {
   @Input() list: Competence[] = [];
   currentCompetence: Competence | undefined;
 
-  constructor(public backgroundColorService: BackgroundColorService) { }
+  constructor(
+    public backgroundColorService: BackgroundColorService,
+    private favoritesService: FavoritesService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -31,6 +35,14 @@ export class CompetencesComponent implements OnInit {
   saveCompetence(competence: Competence) {
     // oops, we don't have id's :D just add it
     this.list.push(competence);
+  }
+
+  favoriteCompetence(competence: Competence) {
+    this.favoritesService.add(competence);
+  }
+
+  unfavoriteCompetence(competence: Competence) {
+    this.favoritesService.remove(competence);
   }
 
   changeBackgroundColor(input: HTMLInputElement) {
